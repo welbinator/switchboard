5a83df637e5c6e35fe70738883ebe6d06a603d0a
James Welbes <james.welbes@gmail.com>
2023-09-05 08:44:13 -0500

    update column logo colors

--- page-templates/home.php
+++ page-templates/home.php
@@ -4,7 +4,7 @@
     
     <div class="column game">
         <div class="gradient"></div>
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>orange-bg-logo.svg" alt="">
+        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>yellow-bg-logo.svg" alt="">
         <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
         <a href="/game" class="custom-link"><div class="overlay">
             <p>The Game</p>
@@ -12,7 +12,7 @@
     </div>
     <div class="column journey">
         <div class="gradient"></div>
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>lime-bg-logo.svg" alt="">
+        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>orange-bg-logo.svg" alt="">
         <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
         <a href="/journey" class="custom-link"><div class="overlay">
             <p>The Journey</p>
@@ -28,7 +28,7 @@
     </div>
     <div class="column events">
         <div class="gradient"></div>
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>teal-bg-logo.svg" alt="">
+        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>blue-bg-logo.svg" alt="">
         <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
         <a href="/events" class="custom-link"><div class="overlay">
             <p>Events</p>
@@ -36,7 +36,7 @@
     </div>
     <div class="column contact-us">
         <div class="gradient"></div>
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>pink-bg-logo.svg" alt="">
+        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>grey-bg-logo.svg" alt="">
         <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
         <a href="/contact-us" class="custom-link"><div class="overlay">
             <p>Contact</p>
