2e07915ee3c60172f7481f98653e6704a0f26353
James Welbes <james.welbes@gmail.com>
2023-09-22 16:29:33 -0500

    fix animation on mobile

--- js/custom-ajax.js
+++ js/custom-ajax.js
@@ -1,64 +1,73 @@
 jQuery(document).ready(function ($) {
-	$('.custom-link').click(function (e) {
-		e.preventDefault();
+    $('.custom-link').click(function (e) {
+        e.preventDefault();
 
-		// Cache the URL of the link
-		const href = $(this).attr('href');
+        // Cache the URL of the link
+        const href = $(this).attr('href');
 
-		// Delay navigation by 500ms
-		setTimeout(function () {
-			window.location.href = href;
-		}, 500);
-	});
+        // Delay navigation by 500ms
+        setTimeout(function () {
+            window.location.href = href;
+        }, 500);
+    });
 
-	// eventlistener for column
-	$( '.custom-link' ).click( function( e ) {
+    // eventlistener for column
+    $('.custom-link').click(function (e) {
         e.preventDefault();
 
         // Cache the clicked column
-        const clickedColumn = $( this ).closest( '.column' );
+        const clickedColumn = $(this).closest('.column');
 
         // Determine the index of the clicked column among all columns
-        const columnIndex = $( '.column' ).index(clickedColumn);
+        const columnIndex = $('.column').index(clickedColumn);
 
         // For each .custom-link
-        $( '.custom-link' ).each( function() {
+        $('.custom-link').each(function () {
             // If the parent .column of the current .custom-link is not the clicked column
-            if ($( this ).closest( '.column' ).get(0) !== clickedColumn.get(0)) {
+            if ($(this).closest('.column').get(0) !== clickedColumn.get(0)) {
                 // Fade it to opacity: 0
-                $( this ).closest( '.column' ).animate( {
+                $(this).closest('.column').animate({
                     opacity: 0
-                }, 500 ); // 500ms duration for the fade animation
+                }, 500); // 500ms duration for the fade animation
             }
         });
 
-        // Determine the translateX value based on the clicked column's index
-        let translateValue = '';
+        let transformValue = '';
         switch (columnIndex) {
-            case 0: // First column
-                translateValue = '0%'; // No transform
+            case 0:
+                transformValue = '0%'; // No transform
                 break;
-            case 1: // Second column
-                translateValue = '-20%';
+            case 1:
+                transformValue = '-20%';
                 break;
-            case 2: // Third column
-                translateValue = '-40%';
+            case 2:
+                transformValue = '-40%';
                 break;
-            case 3: // Fourth column
-                translateValue = '-60%';
+            case 3:
+                transformValue = '-60%';
                 break;
-            case 4: // Fifth column
-                translateValue = '-80%';
+            case 4:
+                transformValue = '-80%';
                 break;
             default:
                 break;
         }
 
-        // Apply the determined translateX value with a transition and a delay
-        $( '#content-area' ).css({
-            'transform': `translateX(${translateValue})`,
-            'transition': 'transform 500ms',
-            'transition-delay': '500ms' // 1 second delay before the transform animation starts
-        });
+        // Check if it's a mobile device based on viewport width
+        if ($(window).width() < 1024) {
+            // For mobile: Move vertically
+            $('#content-area').css({
+                'transform': `translateY(${transformValue})`,
+                'transition': 'transform 500ms',
+                'transition-delay': '500ms'
+            });
+        } else {
+            // For desktop: Move horizontally
+            $('#content-area').css({
+                'transform': `translateX(${transformValue})`,
+                'transition': 'transform 500ms',
+                'transition-delay': '500ms'
+            });
+        }
     });
 });
