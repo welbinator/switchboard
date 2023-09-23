af04ff64f882d135c93120cf977d4eb3a1977211
James Welbes <james.welbes@gmail.com>
2023-09-22 19:24:38 -0500

    fix inner gradients and images

--- page-templates/home.php
+++ page-templates/home.php
@@ -3,8 +3,8 @@
 <main id="content-area">
         <div class="column game">
             <div class="gradient"></div>
-            <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>yellow-bg-logo.svg" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
+            <img class="desktop-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>yellow-bg-logo.svg" alt="">
+            <img class="mobile-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
             <a href="/the-game" class="custom-link">
                 <div class="overlay">
                     <p>The Game</p>
@@ -13,32 +13,32 @@
         </div>
     <div class="column journey">
         <div class="gradient"></div>
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>orange-bg-logo.svg" alt="">
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
+        <img class="desktop-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>orange-bg-logo.svg" alt="">
+        <img class="mobile-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
         <a href="/journey" class="custom-link"><div class="overlay">
             <p>The Journey</p>
         </div></a>
     </div>
     <div class="column press">
         <div class="gradient"></div>
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>purple-bg-logo.svg" alt="">
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
+        <img class="desktop-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>purple-bg-logo.svg" alt="">
+        <img class="mobile-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
         <a href="/press" class="custom-link"><div class="overlay">
             <p>Press</p>
         </div></a>
     </div>
     <div class="column events">
         <div class="gradient"></div>
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>blue-bg-logo.svg" alt="">
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
+        <img class="desktop-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>blue-bg-logo.svg" alt="">
+        <img class="mobile-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
         <a href="/events" class="custom-link"><div class="overlay">
             <p>Events</p>
         </div></a>
     </div>
     <div class="column contact-us">
         <div class="gradient"></div>
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>grey-bg-logo.svg" alt="">
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
+        <img class="desktop-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>grey-bg-logo.svg" alt="">
+        <img class="mobile-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
         <a href="/contact-us" class="custom-link"><div class="overlay">
             <p>Contact</p>
         </div></a>
--- page-templates/inner.php
+++ page-templates/inner.php
@@ -2,10 +2,11 @@
 <?php echo get_header(); ?>
 <main>
 
-    <div class="<?php print strtolower(get_the_title()); ?> column game">
+    <div class="<?php print strtolower(get_the_title()); ?> inner column">
         <div class="gradient"></div>
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>yellow-bg-logo.svg" alt="">
-        <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
+        <!-- <img src="<?php echo get_template_directory_uri() . '/inc/img/' ?>yellow-bg-logo.svg" alt=""> -->
+        <img class="desktop-column-image" src="<?php echo esc_url( get_field('column_image') ); ?>" alt="">
+        <img class="mobile-column-image" src="<?php echo get_template_directory_uri() . '/inc/img/' ?>switchboard-logo-mobile.svg" alt="">
         <div class="overlay">
             <h1><?php the_title(); ?></h1>
         </div>
